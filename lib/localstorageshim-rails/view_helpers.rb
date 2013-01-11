1c1d0793ade5a9b62d7aa2b8c03fe82988e3c980
Nathan Broadbent <nathan.f77@gmail.com>
2013-01-11 20:06:33 +1300

    Add .js ext to localstorageshim asset path

--- lib/localstorageshim-rails/view_helpers.rb
+++ lib/localstorageshim-rails/view_helpers.rb
@@ -3,7 +3,7 @@ module Localstorageshim
     def localstorage_shim
       <<-HTML.strip.html_safe
 <!--[if lte IE 7]>
-    <script src="#{asset_path 'localstorageshim'}" type="text/javascript" id="ie-localstorage-shim"></script>
+    <script src="#{asset_path 'localstorageshim.js'}" type="text/javascript" id="ie-localstorage-shim"></script>
 <![endif]-->
 HTML
     end
